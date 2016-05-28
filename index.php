732121d4f7d11da6c3cf7fd8ce78757e57cc6916
koizoom1 <jigoku.hen@gmail.com>
2016-05-28 17:34:54 +0900

    for debug

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $app->post('/callback', function (Request $request) use ($app) {
         $resContent = $msg['content'];
         //$resContent['text'] = 'ねばぎば！';
         
-        $resContent['text'] = strval($last).$full;
+        $resContent['text'] = strval($last).$result[15];
 
 $resContent['contentType'] = '1';
 
